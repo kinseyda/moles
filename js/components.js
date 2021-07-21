ebed4fffa800c7f950e9dbce1d3a7750310cbf07
kinseyda <dkins090@uottawa.ca>
2021-07-21 16:32:49 -0400

    Add detailed description system
    Upgrades now have a system for displaying costs and effects. This will probably be more generalized more when buildings / producing structures are added.

--- js/components.js
+++ js/components.js
@@ -21,14 +21,67 @@ Vue.component("upgrade-item", {
       return upgradeDict[id];
     },
     upgradeHover(upgrade) {
+      app.upgradeInformationData = upgrade;
       app.descriptionBoxData = this.getUpgradeData(upgrade.id).description;
     },
     resetDescription() {
-      return app.resetDescription();
+      app.upgradeInformationData = undefined;
+      app.resetDescription();
     },
     buyUpgrade(upgrade) {
       upgrade.buy();
     },
   },
   template: `<tr class="list-row upgrade-row" @mouseover="upgradeHover(upgrade)" @mouseleave="resetDescription" @click="buyUpgrade(upgrade)"><td>{{ getUpgradeData(upgrade.id).name }}</td></tr>`,
+});
+
+Vue.component("upgrade-information", {
+  props: ["upgrade"],
+  methods: {
+    formatNumber(num) {
+      return formatNumber(num);
+    },
+    getResourceData(id) {
+      return resourceDict[id];
+    },
+    getUpgradeData(id) {
+      return upgradeDict[id];
+    },
+    getUpgradeType(id) {
+      return this.getUpgradeData(id).effect.func;
+    },
+    getEffectDescription(id) {
+      switch (this.getUpgradeType(id)) {
+        case "addMultiplier":
+          return "Multiplies resource gains:";
+        default:
+          return "Does something?";
+      }
+    },
+  },
+  template: `<div><div class="upgrade-desc" id="cost-container"><h4>Cost</h4><ul><li v-for="id in Object.keys(upgrade.cost)">
+    <p>{{ getResourceData(id).name }}: {{ upgrade.cost[id] }}</p></li></ul></div><div class="upgrade-desc" id="effect-container">
+    <h4>Effect</h4><effect-details v-bind:effect="getUpgradeData(upgrade.id).effect"
+    v-bind:detailedDesc="getEffectDescription(upgrade.id)" v-bind:upgradeType="getUpgradeType(upgrade.id)"></effect-details></div></div>`,
+});
+
+Vue.component("effect-details", {
+  props: ["upgradeType", "effect", "detailedDesc"],
+  methods: {},
+  template: `<div><p>{{ detailedDesc }}</p><multiply-effect v-if="upgradeType=='addMultiplier'" 
+  v-bind:effect="effect"></multiply-effect></div>`,
+});
+
+Vue.component("multiply-effect", {
+  props: ["effect", "detailedDesc"],
+  methods: {
+    formatNumber(num) {
+      return formatNumber(num);
+    },
+    getResourceData(id) {
+      return resourceDict[id];
+    },
+  },
+  template: `<div><ul><li v-for="id in Object.keys(effect.params[0])">
+    <p>{{ getResourceData(id).name }}: +{{ effect.params[0][id]*100 }}%</p></li></ul></div>`,
 });
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: "#app",
   data: {
     descriptionBoxData: defaultDescription,
+    upgradeInformationData: undefined,
     gameData: new Game(Date.now(), startingResources, startingUpgrades),
   },
   methods: {
