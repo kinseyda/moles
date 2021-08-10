fcb091ac119bc8f952d70d208be2b0adf6de570a
kinseyda <dkins090@uottawa.ca>
2021-08-09 20:55:20 -0400

    Separate cost description bullet points into separate component

---
+++ src/components/CostBullet.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+    <p>{{ resource.dataObject.name }}: {{ formatNumber(cost) }}, {{ formatTime(timeUntil()) }}</p></li>
+</template>
+
+<script lang="ts">
+import Resource from '@/js/classes/resources';
+import { formatNumber, formatTime } from '@/js/utils';
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'CostBullet',
+  props: {
+    cost: Number, 
+    resource: Resource,
+  },
+  methods: {
+      formatNumber(num: number) {
+      return formatNumber(num, undefined);
+    },
+    formatTime(num: number) {
+      return formatTime(num);
+    },
+    timeUntil(): number {
+        if(this.cost && this.resource?.amount && this.resource.trueRate) {
+            return Math.max((this.cost - this.resource.amount) / this.resource.trueRate, 0);
+        }
+        return 0;
+    }
+  },
+});
+</script>
--- src/components/PurchaseInformation.vue
+++ src/components/PurchaseInformation.vue
@@ -1,6 +1,7 @@
 <template>
-    <div><div class="purchase-desc" id="cost-container"><h4>Cost</h4><ul><li v-for="id in Object.keys(purchase.dataObject.cost)" :key="id">
-    <p>{{ getResource(id).dataObject.name }}: {{ formatNumber(purchase.trueCost(id)) }}, {{ formatTime(purchase.timeUntil(id)) }}</p></li></ul></div><div class="purchase-desc" id="effect-produce-container">
+    <div><div class="purchase-desc" id="cost-container"><h4>Cost</h4><ul>
+    <CostBullet v-for="id in Object.keys(purchase.dataObject.cost)" :key="id" :resource="getResource(id)" :cost="purchase.trueCost(id)">
+      </CostBullet></ul></div><div class="purchase-desc" id="effect-produce-container">
     <produce-details v-if="purchase._class === 'Structure'" v-bind:structure="purchase"></produce-details>
     <effect-details v-if="purchase._class === 'Upgrade'" v-bind:effect="purchase.dataObject.effect"
     v-bind:detailedDesc="getEffectDescription(purchase)" v-bind:upgradeType="getUpgradeType(purchase)"></effect-details></div></div>
@@ -10,8 +11,8 @@
 import { defineComponent } from 'vue'
 import ProduceDetails from './ProduceDetails.vue'
 import EffectDetails from './EffectDetails.vue'
-import {formatNumber, formatTime} from '../js/utils'
-import { mapState } from 'vuex'
+import CostBullet from './CostBullet.vue'
+import {formatNumber, formatTime } from '../js/utils'
 import Upgrade from '@/js/classes/upgrades'
 import { game } from '@/js/classes/game'
 
@@ -26,6 +27,7 @@ export default defineComponent({
   components: {
     ProduceDetails,
     EffectDetails,
+    CostBullet,
   },
   methods: {
     formatNumber(num: number) {
@@ -38,7 +40,7 @@ export default defineComponent({
       return upgrade.dataObject.effect.func;
     },
     getResource(id: number) {
-      return this.gameData.resourceById(id);
+      return this.gameData.resourceDict[id]
     },
     getEffectDescription(upgrade: Upgrade) {
       switch (this.getUpgradeType(upgrade)) {
